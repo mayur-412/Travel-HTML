559751dd03288d10ccc15f2d0fc20c9c9e384bf1
mayur-412 <mayurzezariya990@gmail.com>
2025-12-01 02:26:43 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+// Show the first tab and hide the rest
+$('.tab ul li:first-child').addClass('active');
+$('.table-box').hide();
+$('.table-box:first').show();
+
+// Click function
+$('.tab ul li').click(function(){
+  $('.tab ul li').removeClass('active');
+  $(this).addClass('active');
+  $('.table-box').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+
+$('.counter-section .owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:1
+        },
+        768:{
+            items:3
+        }
+    }
+})
+
+
+
+
+
+ document.addEventListener(
+           "DOMContentLoaded", () => {
+               new Mmenu( "nav#menu", {
+                  "offCanvas": {
+                     "position": "left"
+                  },
+                  "theme": "light"
+               });
+           }
+            );
+
+
+ $(function() {
+  $('.accordian-title').click(function(j) {
+    
+    var dropDown = $(this).closest('.accordian-box').find('.accordian-content');
+    $(this).closest('.accordian-section').find('.accordian-content').not(dropDown).slideUp();
+    
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).closest('.accordian-section').find('.accordian-title.active').removeClass('active');
+      $(this).addClass('active');
+    }
+    
+    dropDown.stop(false, true).slideToggle();
+    j.preventDefault();
+  });
+});
